63540c68330c453b35f05c5b3135baa6b8647027
mattchen0512 <mattchen0512@gmail.com>
2018-10-30 16:34:20 +0800

    1. Array Ts Import to Component.ts 2. (click) Event Binding

---
+++ AngularCorePratice2018/ClientApp/src/Utility/Mock-heros.ts
@@ -0,0 +1,14 @@
+import { Hero } from '../ViewModel/Hero' //注入Hero 型別
+
+export const HEROES: Hero[] = [ //宣告陣列HEROES , 型別Hero
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
--- AngularCorePratice2018/ClientApp/src/app/heros/heros.component.ts
+++ AngularCorePratice2018/ClientApp/src/app/heros/heros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../ViewModel/Hero';
+import { HEROES } from '../../Utility/Mock-heros';
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
@@ -11,9 +12,13 @@ export class HerosComponent implements OnInit {
     id: 1,
     name:'matt_hero'
   };
-  constructor() { }
+  heroes = HEROES; 
 
+  constructor() { }
   ngOnInit() {
   }
-
+  selectedHero: Hero; //onSelect觸發後,回傳至html的物件
+  onSelect(hero: Hero): void { //Html中所指定之方法,func(variable:Object):funcType
+    this.selectedHero = hero;
+  }
 }
