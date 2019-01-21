ac6bf54f70fc468698c0340bd4bdc30ec160ad51
mattchen0512 <mattchen0512@gmail.com>
2019-01-21 17:09:31 +0800

    Add: RxJs Collection

--- AngularCorePratice2018/ClientApp/src/app/rxjs-collection/rxjs-collection.component.ts
+++ AngularCorePratice2018/ClientApp/src/app/rxjs-collection/rxjs-collection.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, QueryList, ViewChild, ElementRef, Inject } from '@angular/core';
-import { Observable, combineLatest, forkJoin, Subject, interval, timer, fromEvent, empty, of, from, throwError } from 'rxjs';
+import { Observable, combineLatest, forkJoin, Subject, interval, timer, fromEvent, empty, of, from, throwError, concat } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { map, tap, take, switchMap, shareReplay, debounceTime, distinctUntilChanged, filter, combineAll, mapTo, takeUntil, skip, takeLast, bufferTime, bufferCount, distinct, zip, catchError, repeat } from 'rxjs/operators';
+import { map, tap, take, switchMap, shareReplay, debounceTime, distinctUntilChanged, filter, combineAll, mapTo, takeUntil, skip, takeLast, bufferTime, bufferCount, distinct, zip, catchError, repeat, concatAll, concatMap, merge, mergeAll, delay, mergeMap, window, count, switchAll, windowToggle, windowCount, windowTime, windowWhen, groupBy, reduce } from 'rxjs/operators';
 import { UpperCasePipe, JsonPipe } from '@angular/common';
+import { SOURCE } from '@angular/core/src/di/injector';
 @Component({
   selector: 'app-rxjs-collection',
   templateUrl: './rxjs-collection.component.html',
@@ -12,6 +13,8 @@ export class RxjsCollectionComponent implements OnInit {
   @ViewChild('CallTakeUntil') _CallTakeUntil: ElementRef;
   @ViewChild('CallTakeLast') _CallTakeLast: ElementRef;
   @ViewChild('BufferBtnClcik') _BufferBtnClcik: ElementRef;
+  @ViewChild('ClearMerge') _ClearMerge: ElementRef;
+  @ViewChild('ClearWindow') _ClearWindow: ElementRef;
   private readonly API_URL = 'https://api.github.com/users';
   private readonly API_URL2 = 'https://jsonplaceholder.typicode.com/todos/';
   private searchTerms = new Subject<string>(); //宣告一個Subject
@@ -19,7 +22,13 @@ export class RxjsCollectionComponent implements OnInit {
   bufferFuncName: string;
   catchResult: string;
   combineAllResult: Array<string[]>;
+  concatMethods: string;
+  concatResult: string;
   distinctResult: string;
+  groupbyResult: any;
+  mergeMethods: string;
+  mergeResult: string;
+  windowResult: string;
   private _takeUntilButtonEvent = new Subject();
   takeUntilResult: string;
   takeLastResult: string;
@@ -41,7 +50,13 @@ export class RxjsCollectionComponent implements OnInit {
     this.bufferResult = '';
     this.catchResult = '';
     this.combineAllResult = new Array<string[]>();
+    this.concatResult = '';
+    this.concatMethods = '';
     this.distinctResult = '';
+    this.groupbyResult = '';
+    this.mergeMethods = '';
+    this.mergeResult = '';
+    this.windowResult = '';
     this.data$ = this.httpClient.get<any[]>(this.API_URL2).pipe(
       map(data => data.splice(0, 10))
     );
@@ -96,14 +111,14 @@ export class RxjsCollectionComponent implements OnInit {
   CallcatchWithEmpty() {
     //['1', '2', '3', '4', 5] 欲測試必須改為此陣列
     from(['1', '2', '3', '4', 'd']).pipe(
-      zip(interval(300), (x, y) =>this.upperPipe.transform(x)!),
+      zip(interval(300), (x, y) => this.upperPipe.transform(x)!),
       catchError(error => {
         console.log(error);
         return empty();
       })
     ).subscribe(res => {
       this.catchResult += res + ',';
-      }, (err) => { }, () => {
+    }, (err) => { }, () => {
       this.catchResult += 'Complete';
     });
   }
@@ -117,6 +132,54 @@ export class RxjsCollectionComponent implements OnInit {
     );
     const combined = example.pipe(combineAll());
     const subscribe = combined.subscribe(val => this.combineAllResult.push(val));
+  }
+
+  //concat
+  //將兩組Observable合併並且依序輸出
+  CallConcat() {
+    let f = timer(10, 500).pipe(
+      map(r => {
+        return { Source: 'A', value: r }
+      }),
+      take(4)
+    );
+    let g = timer(10, 500).pipe(
+      map(r => {
+        return { Source: 'B', value: r }
+      }),
+      take(4)
+    );
+    concat(f, g).subscribe(res => {
+      this.concatMethods = 'Concat';
+      this.concatResult += 'Source' + res.Source + ':' + res.value + ',';
+    });
+  }
+
+  //concatAll
+  //將多維陣列攤開成一維
+  CallConcatAll() {
+    let SourceA = from([1, 2, 3]);
+    let SourceR = of(SourceA, [[4, 5, 6], [11, 23, 45]]);
+    SourceR.pipe(
+      concatAll()
+    ).subscribe(res => {
+      this.concatMethods = 'ConcatAll';
+      this.concatResult += res + ',';
+    });
+  }
+
+  //concatMap
+  //map+concatAll
+  CallConcatMap() {
+    let SourceA = from([[1, 2, 3]]);
+    let SourceB = from([[4, 5, 6], [7, 8, 9], [10, 11, 12]]);
+    let SourceR = of(SourceA, SourceB);
+    SourceR.pipe(
+      concatMap(e => e.pipe(delay(500)))
+    ).subscribe(res => {
+      this.concatMethods = 'ConcatMap';
+      this.concatResult += res + ',';
+    });
   }
 
   //Distinct - Filter For Array
@@ -169,6 +232,47 @@ export class RxjsCollectionComponent implements OnInit {
     )
     this.funcName = 'map';
   }
+
+  //Merge
+  CallMerge() {
+    this.mergeMethods = 'Merge';
+    let interA = interval(500);
+    let interB = interval(800);
+    interA.pipe(
+      merge(interB),
+      takeUntil(
+        fromEvent(this._ClearMerge.nativeElement, 'click')
+      )
+    ).subscribe(res => {
+      this.mergeResult += res + ',';
+    });
+  }
+
+  //MergeAll
+  CallMergeAll() {
+    this.mergeMethods = 'MergeAll';
+    let SourceA = from([1, 2, 3]).pipe(delay(500));
+    let SourceB = from([4, 5, 6]).pipe(delay(300));
+    let SourceR = of(SourceA, SourceB, [[11, 23, 45]]);
+    SourceR.pipe(
+      mergeAll()
+    ).subscribe(res => {
+      this.mergeResult += res + ',';
+    });
+  }
+
+  //MergeMap
+  CallMergeMap() {
+    this.mergeMethods = 'MergeMap';
+    let SourceA = from([1, 2, 3]).pipe(delay(500));
+    let SourceB = from([4, 5, 6]).pipe(delay(300));
+    SourceA.pipe(
+      mergeMap(obj => SourceB)
+    ).subscribe(res => {
+      this.mergeResult += res + ',';
+    });
+  }
+
   CallswitchMap() {
     this.data$ = this.data$.pipe(
       shareReplay(1),
@@ -188,6 +292,13 @@ export class RxjsCollectionComponent implements OnInit {
     );
     this.funcName = 'combineLatest';
   }
+
+  //debounceTime
+  CalldebounceTime(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  //forkJoin
   CallforkJoin() {
     this.data2$ = this.httpClient.get<any[]>(this.API_URL2);
     this.data3$ = this.httpClient.get<any[]>(this.API_URL);
@@ -197,9 +308,34 @@ export class RxjsCollectionComponent implements OnInit {
       map(([a, b]) => (a.concat(b)))
     );
   }
-  CalldebounceTime(term: string) {
-    this.searchTerms.next(term);
+
+  //GroupBy
+  CallGroupBy() {
+    let people = [
+      { name: 'Anna', score: 100, subject: 'English' },
+      { name: 'Anna', score: 90, subject: 'Math' },
+      { name: 'Anna', score: 96, subject: 'Chinese' },
+      { name: 'Jerry', score: 80, subject: 'English' },
+      { name: 'Jerry', score: 100, subject: 'Math' },
+      { name: 'Jerry', score: 90, subject: 'Chinese' },
+    ];
+    from(people).pipe(
+      zip(interval(300), (x, y) => x),
+      //groupBy 將相同元素集合起來,透過map進行組合輸出
+      groupBy(person => person.name),
+      map(group =>
+        group.pipe(reduce((x, y) => (
+          {
+            name: y.name,
+            score: y.score + x.score
+          })))
+      ),
+      mergeAll()
+    ).subscribe(res => {
+      this.groupbyResult += `{name:${res.name},score:${res.score}}\n`;
+    });
   }
+
   //TakeUntil
   StartTakeUntilInterval() {
     interval(1000).pipe(
@@ -226,8 +362,74 @@ export class RxjsCollectionComponent implements OnInit {
     ).subscribe(res => {
       console.log(res);
       this.takeLastResult += res + ',';
-    }, err => { }, () => {
+    }, () => { }, () => {
       this.takeLastResult += 'Complete';
+    });
+  }
+
+  //Window
+  CallWindow() {
+    //map(obj=>obj.pipe(count())),將1秒內Click次數統合
+    //switchAll 攤平Observable<Observable<T>>後輸出
+     fromEvent(document, 'click').pipe(
+      window(interval(1000)),
+      map(obj => obj.pipe(count())),
+       switchAll(),
+       takeUntil(fromEvent(this._ClearWindow.nativeElement, 'click', () => { this.windowResult = '' }))
+    ).subscribe(res => {
+      this.windowResult += res + ',';
+    });
+  }
+
+  //WindowCount
+  CallWindowCount() {
+    //windowCount 設定每N項目發出後,就觸發一次
+    interval(1000).pipe(
+      windowCount(3),
+      map(()=>'每三次觸發'),
+      takeUntil(fromEvent(this._ClearWindow.nativeElement, 'click', () => { this.windowResult = '' }))
+    ).subscribe(res => {
+      this.windowResult += res + ',';
+    });
+  }
+
+  //WindowTime
+  CallWindowTime() {
+    interval(1000).pipe(
+      windowTime(3000),
+      map(() => '每三秒觸發'),
+      takeUntil(fromEvent(this._ClearWindow.nativeElement, 'click', () => { this.windowResult = '' }))
+    ).subscribe(res => {
+      this.windowResult += res + ',';
+    });
+  }
+
+  //WindowToggle
+  CallWindowToggle() {
+    //啟動後按住mousedown為顯示計算,mouseup為輸出
+    //加入map可針對單次做為統計
+    let MouseDownEvent = fromEvent(document, 'mousedown');
+    let MouseUpEvent = fromEvent(document, 'mouseup');
+    interval(1000).pipe(
+      windowToggle(MouseDownEvent, () => MouseUpEvent),
+      //map(obj => obj.pipe(count())),
+      switchAll(),
+      takeUntil(fromEvent(this._ClearWindow.nativeElement, 'click', () => { this.windowResult = '' }))
+    ).subscribe(res => {
+      this.windowResult += res + ',';
+    });
+  }
+
+  //WindowWhen
+  CallWindowWhen() {
+    //將區間中發出的值收集起來,並於觸發後一次輸出
+    interval(1000).pipe(
+      windowWhen(() => interval(5000)),
+      map(obj => obj.pipe(count())),
+      switchAll(),
+      takeUntil(fromEvent(this._ClearWindow.nativeElement, 'click', () => { this.windowResult = '' }))
+    ).subscribe(res => {
+      this.windowResult +='發出總數:'+ res + ',';
     });
   }
 }
