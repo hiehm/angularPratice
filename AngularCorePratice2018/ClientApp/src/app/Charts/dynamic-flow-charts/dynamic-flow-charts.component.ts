d6098a3aa92d20e3996a55f044e26206aa025e7d
mattchen0512 <mattchen0512@gmail.com>
2020-01-10 15:53:20 +0800

    Modify: 滑鼠移動顯示新增按鈕, calc 定位

--- AngularCorePratice2018/ClientApp/src/app/Charts/dynamic-flow-charts/dynamic-flow-charts.component.ts
+++ AngularCorePratice2018/ClientApp/src/app/Charts/dynamic-flow-charts/dynamic-flow-charts.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'app-dynamic-flow-charts',
-  templateUrl: './dynamic-flow-charts.component.html',
-  styleUrls: ['./dynamic-flow-charts.component.css']
+    selector: 'app-dynamic-flow-charts',
+    templateUrl: './dynamic-flow-charts.component.html',
+    styleUrls: ['./dynamic-flow-charts.component.css']
 })
 export class DynamicFlowChartsComponent implements OnInit {
 
-  constructor() { }
+    constructor(private renderer2: Renderer2) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    //預計寫成 directive
+    mouseenter(el: MouseEvent) {
+        const _srcEl = (el.srcElement as HTMLElement).children[0];
+        this.renderer2.removeClass(_srcEl, 'dis-none');
+    }
+
+    mouseleave(el: MouseEvent) {
+        const _srcEl = (el.srcElement as HTMLElement).children[0];
+        this.renderer2.addClass(_srcEl, 'dis-none');
+    }
 }
